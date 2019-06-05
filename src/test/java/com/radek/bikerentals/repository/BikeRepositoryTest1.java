0682e66648dae90a71691c229f9a31514b1098fc
radek-git <radoslaw.szpionkowski@gmail.com>
2019-06-05 11:54:52 +0200

    first commit

--- src/test/java/com/radek/bikerentals/repository/BikeRepositoryTest1.java
+++ src/test/java/com/radek/bikerentals/repository/BikeRepositoryTest1.java
@@ -1,4 +1,16 @@
 package com.radek.bikerentals.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
 public class BikeRepositoryTest1 {
+
+    @Autowired
+    private BikeRepository bikeRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+
+
 }
